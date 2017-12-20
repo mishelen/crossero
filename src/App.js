0599a732b667a9b47493eff475ed4fbc9a7cc900
Mikhail Gozhy <mikhail.gozhy@gmail.com>
2017-12-21 00:42:23 +0300

    add worlflow configuration to project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-    
     state = {
-        fields: [
-            ['x', 0, 0],
-            [0, 'x', 0],
-            [0, 0, 'x']
-        ],
+        fields: [['x', 0, 0], [0, 'x', 0], [0, 0, 'x']],
         round: 0
     };
-    
+
     renderField = () => {
-        const {fields} = this.state;
-        return fields.map(row => <div>{row.map(
-            cell => <span>{cell}</span>
-        )}</div>)
-        
+        const { fields } = this.state;
+        return fields.map(row => <div>{row.map(cell => <span>{cell}</span>)}</div>);
     };
-    
+
     render() {
-        return (
-            <div>
-                {this.renderField()}
-            </div>
-        );
+        return <div>{this.renderField()}</div>;
     }
 }
 
 export default App;
-
